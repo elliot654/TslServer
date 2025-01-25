5a7c4839cce60e96a7a6bf926c3708e5b7102f7a
elliot <elliot666@zoho.com>
2025-01-25 13:00:53 +0000

    Add project files.

---
+++ TslServer/Program.cs
@@ -0,0 +1,37 @@
+using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+
+namespace TslServer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.WebHost.UseUrls("http://localhost:6969");
+            var app = builder.Build();
+            app.UseWebSockets();
+            app.Map("/ws", async context =>
+            {
+                if (context.WebSockets.IsWebSocketRequest)
+                {
+                    using var ws = await context.WebSockets.AcceptWebSocketAsync();                                      
+                            var message = "hello world";
+                            var bytes = Encoding.UTF8.GetBytes(message);
+                            var arraySegment = new ArraySegment<byte>(bytes, 0, bytes.Length);
+                            if (ws.State == WebSocketState.Open)
+                            {
+                                await ws.SendAsync(arraySegment, WebSocketMessageType.Text, true, CancellationToken.None);
+                            }                            
+                }
+                else
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                }
+            });
+            await app.RunAsync();
+        }
+        
+    }
+}
