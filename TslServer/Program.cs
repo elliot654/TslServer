724010c6bb4e0b414babe7d268740ae869962599
elliot <elliot666@zoho.com>
2025-01-25 13:30:21 +0000

    read changing realtime data from api and only forward updates to the client

--- TslServer/Program.cs
+++ TslServer/Program.cs
@@ -9,6 +9,7 @@ namespace TslServer
         static async Task Main(string[] args)
         {
             using HttpClient client = new HttpClient();
+            string heldData = null;
             var builder = WebApplication.CreateBuilder(args);
             builder.WebHost.UseUrls("http://localhost:6969");
             var app = builder.Build();
@@ -18,17 +19,25 @@ namespace TslServer
                 if (context.WebSockets.IsWebSocketRequest)
                 {
                     using var ws = await context.WebSockets.AcceptWebSocketAsync();
-                    
+                    while (true)
+                    {
                         string x = await processPosts(client);
-                        
+                        if (heldData != x)
+                        {
                             var bytes = Encoding.UTF8.GetBytes(x);
                             var arraySegment = new ArraySegment<byte>(bytes, 0, bytes.Length);
                             if (ws.State == WebSocketState.Open)
                             {
                                 await ws.SendAsync(arraySegment, WebSocketMessageType.Text, true, CancellationToken.None);
                             }
-                           
-
+                            else
+                            {
+                                break;
+                            }
+                            heldData = x;
+                        }
+                        Thread.Sleep(1000);
+                    }
                 }
                 else
                 {
@@ -39,7 +48,7 @@ namespace TslServer
         }
         static async Task<string> processPosts(HttpClient client)
         {
-            var json = await client.GetStringAsync("https://dummyjson.com/posts");
+            var json = await client.GetStringAsync("http://dev-sample-api.tsl-timing.com/sample-data");
             Console.WriteLine(json);
             return json;
         }
